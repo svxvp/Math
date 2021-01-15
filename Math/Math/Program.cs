3c798a373b486451f7efe4ffe3f0e1385bee66ab
svxvp <77195741+svxvp@users.noreply.github.com>
2021-01-15 21:26:56 +0200

    Add files via upload

---
+++ Math/Math/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Math
+{
+    class Program
+    {
+        static void Main()
+        {
+            Complex a = new Complex(3.35, 5.61);
+            Complex b = new Complex(5, 6);
+            Complex c = a.Add(1);
+            Console.WriteLine("("+c.Real + ", " + c.Imaginary+")");
+            Console.ReadKey();
+        }
+    }
+
+    public class Complex
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+        public Complex(double x, double y)
+        {
+            Real = x;
+            Imaginary = y;
+            
+        }
+        public Complex Add(Complex b)
+        {
+            Complex x =new Complex(this.Real+b.Real,this.Imaginary+b.Imaginary);
+            return x;
+        }
+
+        public Complex Add (int a)
+        {
+            Complex x = new Complex(this.Real + a, this.Imaginary + a);
+            return x;
+        }
+    }
+}
